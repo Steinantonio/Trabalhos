06d2dbc959d5b0d9821d65a4dc6a55a7053fc5ed
antonio.stein <antonio.stein@objectedge.com>
2019-06-14 10:50:24 -0300

    final

--- reactTraining/streams/client/src/components/Streams/StreamShow.js
+++ reactTraining/streams/client/src/components/Streams/StreamShow.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import flv from 'flv.js'
 import {connect} from "react-redux";
 import {fetchSingle} from "../actions";
 
 class StreamShow extends React.Component {
-    componentDidMount() {
-        this.props.fetchSingle(this.props.match.params.id);
+    constructor(props) {
+        super(props);
+        
+        this.videoRef = React.createRef()
     };
+    
+    
+    componentDidMount() {
+        const {id} = this.props.match.params;
+        this.props.fetchSingle(id);
+        this.buildPlayer()
+        
+        
+    };
+    
+    componentDidUpdate() {
+        this.buildPlayer();
+    };
+    
+    
+    componentWillUnmount() {
+        this.player.destroy();
+    };
+    
+    buildPlayer = () => {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+        
+        const {id} = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+    };
+    
     
     render() {
         if (!this.props.stream) {
@@ -17,13 +53,16 @@ class StreamShow extends React.Component {
                  </div>
             );
         }
+        
+        
         const {title, description} = this.props.stream;
         return (
-          <div>
-              <h1>{title}</h1>
-              <h5>{description}</h5>
-              
-          </div>
+             <div className={"ui container"}>
+                 <video ref={this.videoRef} style={{width: '100%'}} controls/>
+                 <h1>{title}</h1>
+                 <h5>{description}</h5>
+             
+             </div>
         );
     };
 };
